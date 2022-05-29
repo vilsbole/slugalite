1ff3074d83b38d71ad9ee37c490ba427be4091c4
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2022-05-29 10:44:40 +0200

    Display naive Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Card } from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>Name + pic</li>
+          <li>Season</li>
+          <li>Scarcity</li>
+          <li>Season</li>
+          <li>Team name & picture</li>
+          <li>Shirt Number</li>
+          <li>Age</li>
+          <li>Position</li>
+          <li>Country</li>
+        </ul>
+      </div>
+    </>
+  );
+};
