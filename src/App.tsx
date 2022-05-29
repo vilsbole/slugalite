f10af2988eb36f7e709178c8ad97bd26cfe5d8e7
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2022-05-29 11:04:06 +0200

    Naive router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Card } from "./components";
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Link to="/cards">Cards</Link>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <Card></Card>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<Card />} />
+      </Routes>
+    </Router>
   );
 }
 
